ee5d4dff2d2d649b4892a93c9cfd1b783bbe687e
805380422@qq.com <805380422@qq.com>
2016-06-18 21:18:21 +0800

    添加变量a

--- app/src/main/java/com/support/android/designlibdemo/MainActivity.java
+++ app/src/main/java/com/support/android/designlibdemo/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+int a=0;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
